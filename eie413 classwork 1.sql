760f10d0298762d02e3e76372f60d3dc242b2263
marega23 <oghenekaro.marega@stu.cu.edu.ng>
2025-01-16 16:40:34 +0100

    Add files via upload

---
+++ eie413 classwork 1.sql
@@ -0,0 +1,21 @@
+CREATE TABLE Students (
+    ID INT,
+    Name  VARCHAR(50),
+    MatriculationNumber VARCHAR(20),
+    Gender VARCHAR(7) 
+);
+INSERT INTO Students (ID, Name, MatriculationNumber, Gender) 
+VALUES (200380, 'Christiano Ronaldo', '20CJ000001', 'Male'),
+       (210008, 'Lionel Messi', '21CJ00002', 'Male'),
+       (210004, 'LeBron James', '21CJ00003', 'Male'),
+       (210006, 'Max Vestaphrin', '21CJ00004', 'Male'),
+       (110009, 'Donal Trump', '11CJ00005', 'Male'),
+       (200002, 'Bukayo Saka', '20CJ00006', 'Male'),
+       (290008, 'Lanesra Pot', '13CJ00007', 'Male'),
+       (240013, 'Pep Guardiolla', '24CJ00008', 'Male'),
+       (240006, 'Dora Explora', '20CJ00009', 'Female'),
+       (150009, 'Olivia Thomas', '15CJ00010', 'Female');
+
+	   
+
+
